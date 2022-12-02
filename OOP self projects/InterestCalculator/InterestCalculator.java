0651f75d56e557f54ab0f6e50dc5da7abee03148
Julian Macioce <jjmacioce@hotmail.com>
2022-12-02 11:24:34 -0500

    mismatch exception added
    yearly growth and total growth also added

--- OOP self projects/InterestCalculator/InterestCalculator.java
+++ OOP self projects/InterestCalculator/InterestCalculator.java
@@ -1,29 +1,62 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Locale;
 
 public class InterestCalculator {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        boolean needInput = true;
+        double interestRate = 0, initialSavings = 0;
+        int years = 0;
 
-        System.out.print("Enter initial savings: ");
-        double initialSavings = s.nextDouble();
+        while (needInput) {
+            System.out.print("Enter initial savings: ");
+            try {
+                initialSavings = s.nextDouble();
+                needInput = false;
+            } catch (InputMismatchException e) {
+                s.nextLine();
+            }
+        }
+        needInput = true;
 
-        System.out.print("Enter interest rate (ex. 8.2% = 8.2): ");
-        double interestRate = (s.nextDouble() / 100);
+        while (needInput) {
+            System.out.print("Enter interest rate (ex. 8.2% = 8.2): ");
+            try {
+                interestRate = (s.nextDouble() / 100);
+                needInput = false;
+            } catch (InputMismatchException e) {
+                s.nextLine();
+            }
+        }
+        needInput = true;
 
-        System.out.print("Enter years of interest to accrue: ");
-        int years = s.nextInt();
+        while (needInput) {
+            System.out.print("Enter years of interest to accrue: ");
+            try {
+                years = s.nextInt();
+                needInput = false;
+            } catch (InputMismatchException e) {
+                s.nextLine();
+            }
+        }
 
-        System.out.println("\nAnnual savings for " + years + " years: ");
+        System.out.println("\nAnnual savings for " + years + " years: \n");
 
-        double currSavings = initialSavings;
+        double currSavings = initialSavings, growthTotal = 0, yearlyGrowth = 0;
 
         for (int i = 0; i < years + 1; i++) {
-            if (i > 0) {
-                System.out.printf(Locale.US, "Year: %d   at %.2f%%   is $%,.2f\n", i, interestRate * 100, currSavings);
-            } else if (i == 0) {
+
+            if (i == 0) {
                 System.out.printf(Locale.US, "Starting amount is $%,.2f\n", currSavings);
+            } else if (i > 0) {
+                System.out.printf(Locale.US,
+                        "Year: %d at %.2f%%   $%,.2f\nTotal gain: $%,.2f   Yearly gain: $%,.2f\n\n", i,
+                        interestRate * 100, currSavings, growthTotal, yearlyGrowth);
             }
-            currSavings = currSavings + (currSavings * interestRate);
+            yearlyGrowth = currSavings * interestRate;
+            growthTotal += (currSavings + (currSavings * interestRate)) - currSavings;
+            currSavings += (currSavings * interestRate);
         }
     }
 }
