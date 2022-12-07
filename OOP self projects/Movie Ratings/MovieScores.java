d804b4d27f6d2abb10f8ce2b593949118f0d89f8
Julian Macioce <jjmacioce@hotmail.com>
2022-12-06 22:40:25 -0500

    Update MovieScores.java

--- OOP self projects/Movie Ratings/MovieScores.java
+++ OOP self projects/Movie Ratings/MovieScores.java
@@ -2,6 +2,7 @@ public class MovieScores {
    public static void main(String[] args) {
       Movies Movie1 = new Movies(); // Calls the constructor
       Movies Movie2 = new Movies("Mugen Train", 9);
+      Movies Movie3 = new Movies("Finding Nemo", 8);
 
       Movie1.print();
       System.out.println();
@@ -10,5 +11,6 @@ public class MovieScores {
       Movie1.setRating(8);
       Movie1.print();
       Movie2.print();
+      Movie3.print();
    }
 }
