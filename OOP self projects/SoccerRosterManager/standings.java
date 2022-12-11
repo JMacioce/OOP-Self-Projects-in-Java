53d57a769080da5eb92814882179aac72d348d45
Julian Macioce <jjmacioce@hotmail.com>
2022-12-11 10:27:58 -0500

    Create standings.java

---
+++ OOP self projects/SoccerRosterManager/standings.java
@@ -0,0 +1,10 @@
+public class standings {
+    team[] s = new team[4];
+
+    public void setTeams() {
+        s[0] = new team("Bay City Bruisers", 88);
+        s[1] = new team("Choco Mountain Rockies", 84);
+        s[2] = new team("Hotel California Sound Check", 78);
+        s[3] = new team("Rapture Kindlies", 91);
+    }
+}
