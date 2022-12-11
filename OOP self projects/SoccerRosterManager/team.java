2d880a16a2ec436a49ca486cbbb188c286b64a5d
Julian Macioce <jjmacioce@hotmail.com>
2022-12-10 21:13:05 -0500

    building team class
    can now save name

---
+++ OOP self projects/SoccerRosterManager/team.java
@@ -0,0 +1,11 @@
+public class team {
+    private String teamName;
+
+    public team(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String toString() {
+        return (teamName);
+    }
+}
