a58ce561142b36cfc0cb3dc6ce2b0efeb699158a
Julian Macioce <jjmacioce@hotmail.com>
2022-12-17 13:03:44 -0500

    Added 2 divisions

--- OOP self projects/SportsTeamManager/standings.java
+++ OOP self projects/SportsTeamManager/standings.java
@@ -1,14 +1,22 @@
 public class standings {
+    public static team[] s2 = new team[6];
     public static team[] s = new team[6];
     public static match[] games = new match[100];
 
     public static void setTeams() {
-        s[0] = new team("Bay City Bruisers", 88);
-        s[1] = new team("Choco Mountain Rockies", 84);
-        s[2] = new team("Hotel California Sound Check", 78);
-        s[3] = new team("Rapture BioShock", 91);
-        s[4] = new team("Nook Inc. Tanukis", 85);
-        s[5] = new team("Lavender Town Cubones", 82);
+        s[0] = new team("Troia Chocobos", 84);
+        s[1] = new team("Green Cap Mansions", 85);
+        s[2] = new team("Giga Impact Snorlaxs", 93);
+        s[3] = new team("PK Starstorms", 90);
+        s[4] = new team("Roma DaVincis", 87);
+        s[5] = new team("Shadow Moses Island Snakes", 91);
+
+        s2[0] = new team("Bay City Bruisers", 88);
+        s2[1] = new team("Choco Mountain Rockies", 84);
+        s2[2] = new team("Hotel California Sound Check", 78);
+        s2[3] = new team("Rapture BioShock", 91);
+        s2[4] = new team("Nook Inc. Tanukis", 85);
+        s2[5] = new team("Lavender Town Cubones", 82);
     }
 
     public static void sortTeamsByRating(team[] s) {
@@ -40,7 +48,7 @@ public class standings {
         }
     }
 
-    public static void callMatchEngine() {
+    public static void callMatchEngine(team[] s) {
         games[0] = new match(s[0], s[1]);
         games[1] = new match(s[2], s[3]);
         games[2] = new match(s[4], s[5]);
@@ -65,23 +73,36 @@ public class standings {
     public static void main(String[] args) {
         setTeams();
         sortTeamsByRating(s);
+        sortTeamsByRating(s2);
 
         System.out.println("Teams Sorted by Rating: ");
+        System.out.println("DIVISION 1 ");
         for (team name : s) {
+            System.out.println(name);
+        }
+        System.out.println();
+        System.out.println("DIVISION 2 ");
+        for (team name : s2) {
             System.out.println(name);
         }
         System.out.println();
 
         for (int i = 1; i < 5; i++) {
-            callMatchEngine();
+            callMatchEngine(s);
             sortTeamsByPoints(s);
+            callMatchEngine(s2);
+            sortTeamsByPoints(s2);
             System.out.println("Standings Round " + i + ": ");
+            System.out.println("DIVISION 1 ");
             for (team name : s) {
                 name.printRecords();
             }
             System.out.println();
+            System.out.println("DIVISION 2 ");
+            for (team name : s2) {
+                name.printRecords();
+            }
+            System.out.println();
         }
-
     }
-
 }
